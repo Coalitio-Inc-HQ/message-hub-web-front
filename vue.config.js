bdbd4f44d627b08a9acb26609648cb50069b2cf7
WhoamiTitle <137419138+WhoamiTitle@users.noreply.github.com>
2024-07-14 00:59:45 +0900

    добавлена замена URL WebSocket в vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          VUE_APP_WS_URL: JSON.stringify(
+            process.env.VUE_APP_API_BASE_URL.replace(/^https/, 'wss') + '/ws_listener'
+          )
+        }
+      })
+    ]
+  }
+});
+
 
 // module.exports = {
 //   // devServer: {
