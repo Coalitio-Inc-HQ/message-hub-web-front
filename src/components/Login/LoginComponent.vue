98ae039baab23d5489ae2504cb6746025cf8698a
WhoamiTitle <137419138+WhoamiTitle@users.noreply.github.com>
2024-07-22 23:06:55 +0900

    Вынесена логика регистрации и авторизации в файл authService.js
     Теперь доступна по импорту: import { request_login, ...} from '@/services/authService';

--- src/components/Login/LoginComponent.vue
+++ src/components/Login/LoginComponent.vue
@@ -1,45 +1,24 @@
 <template>
   <div class="login-body">
     <div :class="['login-container', {'expanded': registerActive}]">
-      <!-- <login-form      
-        v-if="!registerActive"  
-      :formData="loginData"
-        @update:formData="updateLoginData"
-        :onLogin="login"
-        :toggleRegister="toggleRegister"
-      /> -->
-
       <login-form      
         :formData="loginData"
         @update:formData="updateLoginData"
         :onLogin="login"
         :toggleRegister="toggleRegister"
       />
-      <!-- <register-form
-        v-else
-        :formData="registerData"
-        @update:formData="updateRegisterData"
-        :onRegister="register"
-        :toggleRegister="toggleRegister"
-      /> -->
     </div>
   </div>
 </template>
 
 <script>
 import LoginForm from './LoginForm.vue';
-//import RegisterForm from './RegisterForm.vue';
-import axios from 'axios';
-import router from "@/router";
+import { request_login } from '@/services/authService';
 
-const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
-const AUTH_REGISTER_URL = `${API_BASE_URL}${process.env.VUE_APP_AUTH_REGISTER_URL}`;
-const AUTH_LOGIN_URL = `${API_BASE_URL}${process.env.VUE_APP_AUTH_LOGIN_URL}`;
 
 export default {
   components: {
-    LoginForm,
-    //RegisterForm
+    LoginForm
   },
   data() {
     return {
@@ -47,13 +26,7 @@ export default {
       loginData: {
         username: '',
         password: ''
-      },
-      // registerData: {
-      //   usernameReg: '',
-      //   emailReg: '',
-      //   passwordReg: '',
-      //   confirmPasswordReg: ''
-      // }
+      }
     };
   },
   mounted(){
@@ -63,82 +36,14 @@ export default {
   },
   methods: {
     login() {
-      // if (!this.validateLoginPassword()) {
-      //   alert('Пароль должен содержать минимум 8 символов');
-      //   return;
-      // }
-      this.request_login(this.loginData.username, this.loginData.password)
+      request_login(this.loginData.username, this.loginData.password, this.set_cookie);
     },
     toggleRegister() {
       this.registerActive = !this.registerActive;
     },
-    validateEmail(email) {
-      const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
-      return re.test(email);
-    },
-    // validateLoginPassword() {
-    //   const minLength = 8;
-    //   return this.loginData.password.length >= minLength;
-    // },
-    validateRegistrationPassword(value) {
-      const minLength = 8;
-      return value.length >= minLength;
-    },
-    register() {
-      if (!this.validateRegistrationPassword(this.registerData.passwordReg)) {
-        alert('Пароль должен содержать минимум 8 символов');
-        return;
-      } else if (!this.validateEmail(this.registerData.emailReg)) {
-        alert("Пожалуйста, введите корректный email.");
-        return;
-      } else if (this.registerData.passwordReg !== this.registerData.confirmPasswordReg) {
-        alert("Пароли не совпадают. Пожалуйста, повторите попытку.");
-        return;
-      }
-      let temp_emailReg = this.registerData.emailReg;
-      let temp_passwordReg = this.registerData.passwordReg;
-      let metod = this.request_login;
 
-      axios.post(AUTH_REGISTER_URL, {
-        name: this.registerData.usernameReg,
-        email: this.registerData.emailReg,
-        password: this.registerData.passwordReg
-      })
-      .then(function (response) {
-        if (response.status == 201){
-          localStorage.already_registered = true;
-          metod(temp_emailReg, temp_passwordReg);
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    },
-    request_login(email, password){
-      let setcookie = this.setCookie;
-      axios.post(AUTH_LOGIN_URL, {
-        username: email,
-        password: password
-      }, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        withCredentials: true
-      })
-      .then(function (response) {
-        if (response.status >= 200 && response.status < 300){
-          if (response.data.access_token){
-            setcookie("token", response.data.access_token, 1)
-            router.push('/chat');
-          }
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert("Неправильный логин или пароль. Пожалуйста, повторите попытку.");
-      });
-    },
-    setCookie(name, value, days) {
+
+    set_cookie(name, value, days) {
       var expires = "";
       if (days) {
         var date = new Date();
@@ -160,14 +65,6 @@ export default {
 </script>
 
 
-<!-- .login-container {
-  transition: height 0.5s ease;
-  overflow: hidden;
-}
-
-.login-container.expanded {
-  height: 360px;
-} -->
 <style scoped>
 @import '@/assets/LoginComponent.css';
 </style>
