4de694d890c217902ed8cd633171f0474aafbe76
WhoamiTitle <137419138+WhoamiTitle@users.noreply.github.com>
2024-07-23 16:26:37 +0900

    chore

--- src/services/dateUtils.js
+++ src/services/dateUtils.js
@@ -1,6 +1,5 @@
 // services/dateUtils.js
 const moment = require('moment-timezone');
-// Функция для конвертации времени в UTC с учетом разницы между локальным временем и UTC
 
 export function extract_time_from_timestamp_handler(timestamp) {
   const time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -14,9 +13,8 @@ export function extract_time_from_timestamp_handler(timestamp) {
       adjusted_local_date = local_date.subtract(Math.abs(offset_in_hours), 'hours');
   }
 
-
   const adjusted_local_timeString = adjusted_local_date.format('YYYY-MM-DDTHH:mm:ss.SSSZ');
-  //console.log("Локальное время после корректировки:", adjusted_local_timeString);
+
   return adjusted_local_timeString;
 }
 
@@ -24,8 +22,6 @@ export function format_time_for_display(timestamp) {
   const time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const local_date = moment.tz(timestamp, time_zone);
   const local_TimeString = local_date.format('HH:mm');
-  //const timezone_Offset = local_date.format('Z');
-  //console.log("Смещение относительно UTC:", `UTC${timezone_Offset}`);
 
   return local_TimeString;
 }
